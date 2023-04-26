b777bf93e401199c4f3a8e18784c18c14e1e0960
Cristhian González <cristhian.gonzalez@udea.edu.co>
2023-04-25 22:11:54 -0500

    changing the root message

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Testing the entire pipeline v2!!!",
+			"message": "Hello from AWS with Jenkins!!!",
 		})
 	})
 

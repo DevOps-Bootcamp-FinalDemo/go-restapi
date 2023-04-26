6d697d3d2cf7da443743070230e89da20f3fe9fa
Cristhian González <cristhian.gonzalez@udea.edu.co>
2023-04-26 10:32:40 -0500

    editing main message

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello from AWS with Jenkins Definitive Edition!!!",
+			"message": "Hello from AWS with Jenkins, SUPER Definitive Edition!!!",
 		})
 	})
 
